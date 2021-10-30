9e3b61d9ceb0899f3ff761d61210d671cd88c4b5
Pierre Emmanuel CHAT <pe_chaut@hotmail.com>
2021-10-30 10:10:57 +0200

    Updates

---
+++ precache-manifest.3ddcda6512fb0226102589fa37b42255.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4568e01230d4ca64e189ab64edfebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b589e4dabff047633a",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "045371053a7551c6bcf2",
+    "url": "/static/css/main.bb1cbc47.chunk.css"
+  },
+  {
+    "revision": "47b589e4dabff047633a",
+    "url": "/static/js/2.6d9dcc3f.chunk.js"
+  },
+  {
+    "revision": "4f966ab26f9b7cd9258b5df9a3f143db",
+    "url": "/static/js/2.6d9dcc3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045371053a7551c6bcf2",
+    "url": "/static/js/main.c9a392c1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
